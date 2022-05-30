6fc7258c2c16100b3718330055710f392b5cb2f3
safouene <safouene-abbachi@hotmail.fr>
2022-05-30 18:11:17 +0100

    DEV : Adding the transition animation logic

--- DrumKit/script.js
+++ DrumKit/script.js
@@ -1,6 +1,19 @@
-window.addEventListener('keydown', (e) => {
+function playAudio(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  if (!audio) return; // if no sound exit the function
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.play(); // if no sound exit the function
   audio.currentTime = 0; // rewind to the start
-  audio.play();
-});
+  key.classList.add('playing');
+}
+function removeTransition(event) {
+  if (event.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+const allKeys = document.querySelectorAll('.key');
+
+allKeys.forEach((element) =>
+  element.addEventListener('transitionend', removeTransition)
+);
+window.addEventListener('keydown', playAudio);
